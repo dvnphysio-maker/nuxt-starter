a4e2743aab5f74f4fdff9ed96d8a2bacbf85bd91
dvnphysio-maker <dvnphysio@gmail.com>
2025-09-21 20:19:08 +0100

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="my-20">
     <p class="text-sm text-center text-zinc-600">
-      Copyright &copy; {{ new Date().getFullYear() }} Nuxt Starter. Powered by <a href="https://www.netlify.com/" class="underline">Netlify</a>.
+      Copyright &copy; {{ new Date().getFullYear() }} Devon Stroke & Neuro Physio. Registered witht the  <a href="https://www.hcpc-uk.org/check-the-register/professional-registration-detail/?query=PH88629&profession=PH&backtosearchurl=https://www.hcpc-uk.org/check-the-register/register-results/&backtosearchquery=conibere" class="underline">HCPC</a>.
     </p>
   </footer>
 </template>
