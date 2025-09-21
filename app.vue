6c6d4f094ee1eee5b531e2c7e7f2697845f1bcee
dvnphysio-maker <dvnphysio@gmail.com>
2025-09-21 22:34:58 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@ useHead({
     {
       name: "description",
       content:
-        "Expert Physiotherapy for Stroke and Neurological Rehabilitation in Devon.",
+        "Expert Physiotherapy for Stroke and Neurological Rehabilitation in Devon. Providing personalised patient centred care to enable your recovery",
     },
   ],
   bodyAttrs: {
